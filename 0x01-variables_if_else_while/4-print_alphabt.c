fe101b7fe5c5233c1ac75c33ff2bbcddbfc3c34e
Kingyaga <99516275+Kingyaga@users.noreply.github.com>
2022-06-03 20:47:58 +0100

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,14 +8,14 @@
  * Return: Always 0 (Success)
  */
 
-int main (void)
+int main(void)
 {
 	char ch, e, q;
 
 	e = 'e';
 	q = 'q';
 
-	for ( ch = 'a'; ch <= 'z'; ch++)
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		if (ch != e && ch != q)
 			putchar(ch);
