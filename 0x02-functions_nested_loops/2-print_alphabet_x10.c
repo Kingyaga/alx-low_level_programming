9b85990c02ef4930b46f71b4e38dc167bcd7408f
Kingyaga <99516275+Kingyaga@users.noreply.github.com>
2022-06-07 19:43:12 +0100

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Print 10x the alphabet followed by a new line.
- *  print_alphabet_x10 - Entry Point
+ * print_alphabet_x10 - Entry Point
  * Return: 0
  */
 void print_alphabet_x10(void)
