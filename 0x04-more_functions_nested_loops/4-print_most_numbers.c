9d5821181c40a8d9fce9ba65c36444363e86dc81
Kingyaga <marcellusobiajulu@gmail.com>
2022-06-09 08:05:33 -0700

    update

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_most_numbers - printing numbers.
+ * Return: void.
+ */
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+{
+	if (i != 2 && i != 4)
+
+	_putchar(i + 48);
+}
+	_putchar('\n');
+}
