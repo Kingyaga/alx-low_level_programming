13e4d093e8df06dda511e75939a9b364c10ef06a
Kingyaga <99516275+Kingyaga@users.noreply.github.com>
2022-06-09 13:36:54 +0100

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,7 @@
 #include "main.h"
 /**
  * _isupper - checks for the case of the alphabet.
- *
- * @c - checked character.
- *
+ * @c : checked character.
  * Return: 1 if c is upper, 0 if c is lower.
  */
 int _isupper(int c)
