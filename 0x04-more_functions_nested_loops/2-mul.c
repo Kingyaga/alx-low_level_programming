d405e8e37f601731261433af8f930a09542f8a46
Kingyaga <99516275+Kingyaga@users.noreply.github.com>
2022-06-09 14:08:25 +0100

    Update 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,11 +1,13 @@
 #include "main.h"
 /**
  * mul - multiply two ints.
- *
+ *@a : int a.
+ *@b : int b.
  * Return: multi.
  */
 int mul(int a, int b)
 {
 	int multi = a * b;
+	
 	return (multi);
 }
