d971ad233274ae8202c5910b8825c5a8a1046a72
Kingyaga <marcellusobiajulu@gmail.com>
2022-06-09 05:32:43 -0700

    cases

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _isupper - checks for the case of the alphabet.
+ *
+ * @c - checked character.
+ *
+ * Return: 1 if c is upper, 0 if c is lower.
+ */
+int _isupper(int c)
+{
+	return (((c > 96 && c < 123)) ? 1 : 0);
+}
