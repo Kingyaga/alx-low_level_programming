3e30f1f9a5ad2a194d5d69700345b89ee5bf17ae
Kingyaga <99516275+Kingyaga@users.noreply.github.com>
2022-06-15 20:45:39 +0100

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,20 +1,20 @@
 #include "main.h"
 /**
- * string_toupper - a function that changes a string to upper case
- * @a : string array
- * Return:char
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @s: input string.
+ * Return: the pointer to dest.
  */
-char *string_toupper(char *a)
-{
-	int i = 0;
 
-	while (a[i] != '\0')
+char *string_toupper(char *s)
+{
+	int count = 0;
+
+	while (*(s + count) != '\0')
 	{
-		if (a[i] >= 'a' && a[i] <= 'z')
-		{
-			a[i] = a[i] - 32;
-		}
-		i++;
+		if ((*(s + count) >= 97) && (*(s + count) <= 122))
+			*(s + count) = *(s + count) - 32;
+		count++;
 	}
-	return (a);
+
+	return (s);
 }
