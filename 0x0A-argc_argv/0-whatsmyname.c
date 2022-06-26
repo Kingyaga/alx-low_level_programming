a432930f73917aaab45cf60870f2b86122f18fd2
Kingyaga <99516275+Kingyaga@users.noreply.github.com>
2022-06-26 22:03:04 +0100

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,21 @@
 #include "main.h"
 #include <stdio.h>
-void main (int argc, char *argv[])
+/**
+ * main - Entry point
+ *
+ * @argc: Argument Counter
+ *
+ * @argv: Argument Vector
+ *
+ * Return: Always (0)
+ */
+
+int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
+	if (argc >= 0)
+	{
+		printf("%s\n", argv[0]);
+	}
+
 	return (0);
 }
